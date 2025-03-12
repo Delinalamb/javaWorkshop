9f353f1afb3f57bbdf0f470dfb35a6947ce1ec1d
Delinalamb <delinalamnamayum@gmail.com>
2025-03-12 14:29:31 +0530

    Forth code

---
+++ Method/Add.java
@@ -0,0 +1,15 @@
+public class Add
+{
+    public int add(int A, int B)
+    {
+        return A+B;}
+        public static void main(String args[])
+        {
+            Add obj=new Add();
+            int C=obj.add(10,20);
+            System.out.println(C);
+
+        }
+    
+}
+    
