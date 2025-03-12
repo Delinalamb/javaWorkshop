9232948932bc12dc4a0eac9d6e0a163e65815689
Delinalamb <delinalamnamayum@gmail.com>
2025-03-12 14:37:04 +0530

    return type done

---
+++ ReturnType/Adding.java
@@ -0,0 +1,15 @@
+public class Adding
+{
+    public int add(int A, int B)
+    {
+        return A+B;}
+        public static void main(String args[])
+        {
+            Adding obj=new Adding();
+            int C=obj.add(10,20);
+            System.out.println(C);
+
+        }
+    
+}
+    
