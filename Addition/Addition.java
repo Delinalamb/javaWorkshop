9c89b0dae78d631bdcf6b0fdbdc6b618c069fc00
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 15:23:50 +0530

    Addition done

---
+++ Addition/Addition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Addition
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number one:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number two:");
+        int num2 = sc.nextInt();
+        System.out.println(num1+num2);
+
+    }
+
+}
