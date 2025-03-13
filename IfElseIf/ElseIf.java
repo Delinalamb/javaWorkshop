b911a8eae56e571ebac50914964c7846e57aad52
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 11:52:43 +0530

    IfElseIf done

---
+++ IfElseIf/ElseIf.java
@@ -0,0 +1,19 @@
+public class ElseIf
+{
+    public static void main(String args[])
+    {
+        int num=25;
+        if(num>0)
+        {
+            System.out.println("it is negative");
+        }
+        else if(num==0)
+        {
+            System.out.println("it is nutral");
+        }
+        else
+        {
+            System.out.println("it is positive");
+        }
+    }
+}
