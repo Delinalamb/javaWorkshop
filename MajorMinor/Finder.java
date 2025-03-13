8cad9d02664eb919ffe4912e746ec4132a8bff8b
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 11:09:11 +0530

    MajorMinor done

---
+++ MajorMinor/Finder.java
@@ -0,0 +1,15 @@
+public class Finder
+{
+    public static void main(String args[])
+    {
+        int age=25;
+        if(age>=18)
+        {
+            System.out.println("She is major");
+        }
+        else{
+            System.out.println("She is minor");
+
+        }
+    }
+}
