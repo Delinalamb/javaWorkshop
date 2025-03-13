eebd560a090672a451285a4851c61f887dfc6fbe
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 11:15:22 +0530

    positive negative done

---
+++ PositiveNegative/PositiveNegative.java
@@ -0,0 +1,15 @@
+public class PositiveNegative
+{
+    public static void main(String args[])
+    {
+        int num=25;
+        if(num>=0)
+        {
+            System.out.println("it is positive");
+        }
+        else{
+            System.out.println("it is negative");
+
+        }
+    }
+}
