07edd2f8eb4813c18e1fc76f9cad11fce391458d
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 14:01:49 +0530

    3D done

---
+++ 3D/MultiArr.java
@@ -0,0 +1,8 @@
+public class MultiArr
+{
+    public static void main(String args[])
+    {
+        int a[][][] ={{{10,20}},{{10,20},{30,100,45}},{{20,30}},{{40,50}}};
+        System.out.println(a[1][1][1]);
+    }
+}
