54bf730100300f340f092832555176c418ab5acc
Delinalamb <delinalamnamayum@gmail.com>
2025-03-13 10:58:50 +0530

    ifelse done

---
+++ IfElse/Prog.java
@@ -0,0 +1,17 @@
+public class Prog
+{
+    public static void main(String args[])
+    {
+        if (false){
+            System.out.println("yes");
+        }
+        else{
+             System.out.println("no");
+
+
+        }
+
+        
+
+    }
+}
