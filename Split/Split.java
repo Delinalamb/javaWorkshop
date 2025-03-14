0d9ebd9b2fcd67417d2d98a91ada8615af4fe58c
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 11:52:38 +0530

    Split done

---
+++ Split/Split.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Split
+{
+    public static void main(String arg[])
+    {
+        String name ="Hello,student";
+        String names[]=name.split(",");
+        System.out.println(names[0]);
+    }
+}
