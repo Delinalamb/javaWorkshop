dcd5f819ffe253360efe7d9faf6dbc95c4009978
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 12:03:37 +0530

    UpperLower done

---
+++ Case/Case.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Case
+{
+    public static void main(String arg[])
+    {
+        String name ="HELLO STUDENT";
+        System.out.println(name.toLowerCase());
+         System.out.println(name.toUpperCase());
+    }
+}
