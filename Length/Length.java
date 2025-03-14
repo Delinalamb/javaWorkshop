b31aef522523ebaf1131df16c58c81725b2b04b3
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 11:56:28 +0530

    Length done

---
+++ Length/Length.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Length
+{
+    public static void main(String arg[])
+    {
+        String name ="Hello student";
+        System.out.println(name.length());
+    }
+}
