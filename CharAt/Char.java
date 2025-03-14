ce91f7231f671b8e059bb2910c3f7cdacef682c8
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 11:42:04 +0530

    CharAt done

---
+++ CharAt/Char.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Char
+{
+    public static void main(String arg[])
+    {
+        String name ="Hello student";
+        System.out.println(name.charAt(0));
+    }
+}
