bf71b26c9b88f4be2bb1af544bbd294f807d0b15
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 12:09:01 +0530

    Index done

---
+++ Index/Index.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Index
+{
+    public static void main(String arg[])
+    {
+        String name ="Hello student";
+        System.out.println(name.indexOf("s"));
+    }
+}
