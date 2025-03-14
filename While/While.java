8e01b9278d7d14c1d262006fe569c56520a9ce93
Delinalamb <delinalamnamayum@gmail.com>
2025-03-14 10:03:27 +0530

    While done

---
+++ While/While.java
@@ -0,0 +1,13 @@
+
+public class While
+{
+    public static void main(String args[])
+    {
+        int i=92;
+        while(i<98)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
