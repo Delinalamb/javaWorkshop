89bac21292661de319f9ef181c10f22fde2f6403
Delinalamb <delinalamnamayum@gmail.com>
2025-03-15 10:37:54 +0530

    Constructor done

---
+++ Constructor/Constructor.java
@@ -0,0 +1,11 @@
+public class Constructor
+{
+    Constructor()
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String args[])
+    {
+        Constructor ob=new Constructor();
+    }
+}
